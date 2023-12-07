76e5cb82818b6fe73a2f31d6d867bdf7a85e9c00
rymdcowboy <69717496+rymdcowboy@users.noreply.github.com>
2023-12-07 08:29:43 +0100

    Update program.py

--- program.py
+++ program.py
@@ -8,12 +8,13 @@ __email__ = "christian.andersson.2@stud.ki.se"
 import os
 import csv
 
-PROJECT_FOLDER_PATH = ""
+PROJECT_FOLDER_PATH = "C:\\Users\\Christian\\Documents\\Tutor\\"
 DATASETS_FILE_NAME = "open_me.mrb"
 BIG_BRAIN_FILE_NAME = "Big_brain.nrrd"
 IN_VIVO_FILE_NAME = "In_vivo.nrrd"
 EX_VIVO_FILE_NAME = "Synthesized_FLASH25_in_MNI_v2_500um.nii"
 STUDENT_STRUCTURES_FILE_NAME = "G_VT23_practical_dis_MRI_.csv"
+MARKUP_PATH = "G:\\Students\\"
 
 BIG_BRAIN = "Big_Brain"
 IN_VIVO = "in_vivo"
@@ -179,7 +180,7 @@ def inputNumberInRange(prompt, low, high, exceptions=list()):
 
 # Sparar en nod med control points till en fil
 def saveNodeToFile(exam_nr, node):
-    slicer.util.saveNode(node, PROJECT_FOLDER_PATH + f"{exam_nr}.mrk.json") # eller mkp.json
+    slicer.util.saveNode(node, MARKUP_PATH + f"{exam_nr}.mrk.json") # eller mkp.json
 
 # Laddar in en fil med markups
 def loadNodeFromFile(path):
@@ -192,6 +193,7 @@ def main():
         # Återställer fönstrena och byter till big brain vid ny användare
         resetWindow()
         resetAnsweredQuestions()
+        # definera filename här
         print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 
         # readExamNr()
@@ -219,11 +221,11 @@ def main():
                 break
             if is_correct_exam_nr == 2:
                 continue
-        if os.path.isfile(PROJECT_FOLDER_PATH + f"{exam_nr}.mrk.json"):
+        if os.path.isfile(MARKUP_PATH + f"{exam_nr}.mrk.json"):
             print(f"En fil med markups existerar redan för exam nr {exam_nr}")
             read_file_option = inputNumberInRange("Vill du läsa in den?\n1 - Ja\n2 - Nej\n", 1, 2)
             if read_file_option == 1:
-                node = loadNodeFromFile(PROJECT_FOLDER_PATH + f"{exam_nr}.mrk.json")
+                node = loadNodeFromFile(MARKUP_PATH + f"{exam_nr}.mrk.json")
             elif read_file_option == 2:
                 node = addNodeAndControlPoints(exam_nr, structures)
                 pass
